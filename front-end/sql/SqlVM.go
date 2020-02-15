6b3c30bfd91c335b2dd0f45bcc73cb35f2b8d371
MathxH Chen <brainfvck@foxmail.com>
2020-02-15 17:22:38 +0800

    Add SQL VM module and its unit test

---
+++ front-end/sql/SqlVM.go
@@ -0,0 +1,74 @@
+package sql
+
+import (
+	"fmt"
+	"os"
+	"tiny-rdb/front-end/cli"
+	"tiny-rdb/util"
+)
+
+// const Result var
+const (
+	// Raw Command Result
+	RawCommandSuccess         = iota
+	RawCommandUnrecognizedCMD = iota
+
+	// Prepare Statement Reuslt
+	PrepareSuccess               = iota
+	PrepareUnrecognizedStatement = iota
+
+	// Satement Type
+	InsertStatement = iota
+	SelectStatement = iota
+)
+
+// StatementType type of statement
+type StatementType = int
+
+// PrepareStatementResult result of statement
+type PrepareStatementResult = int
+
+// RawCommandResult result of raw command
+type RawCommandResult = int
+
+// Statement represent a statment
+type Statement struct {
+	Type StatementType
+}
+
+// RunRawCommand Run raw command
+func RunRawCommand(inputBuffer *cli.InputBuffer) RawCommandResult {
+	if inputBuffer.Buffer == "exit" || inputBuffer.Buffer == "quit" {
+		os.Exit(util.ExitSuccess)
+	} else {
+		return RawCommandUnrecognizedCMD
+	}
+	return RawCommandUnrecognizedCMD
+}
+
+// PrepareStatement Prepare statement
+func PrepareStatement(inputBuffer *cli.InputBuffer, statement *Statement) PrepareStatementResult {
+	if inputBuffer.Buffer == "insert" {
+		statement.Type = InsertStatement
+		return PrepareSuccess
+	}
+
+	if inputBuffer.Buffer == "select" {
+		statement.Type = SelectStatement
+		return PrepareSuccess
+	}
+
+	return PrepareUnrecognizedStatement
+}
+
+// RunStatement Run statement
+func RunStatement(statement *Statement) {
+	switch statement.Type {
+	case InsertStatement:
+		// TODO: Insert
+	case SelectStatement:
+		// TODO: Select
+	default:
+		fmt.Println("Unkown Statement.")
+	}
+}
---
+++ front-end/sql/SqlVM_test.go
@@ -0,0 +1,56 @@
+package sql
+
+import (
+	"testing"
+	"tiny-rdb/front-end/cli"
+)
+
+func TestRunRawCommand(t *testing.T) {
+	inputBuffer := cli.NewInputBuffer()
+	inputBuffer.Buffer = "testCmd"
+	inputBuffer.BufLen = len(inputBuffer.Buffer)
+
+	if RunRawCommand(inputBuffer) != RawCommandUnrecognizedCMD {
+		t.Errorf("Command is unrecognized command")
+	}
+}
+
+func TestPrepareStatement(t *testing.T) {
+	inputBuffer := cli.NewInputBuffer()
+	inputBuffer.Buffer = "insert"
+	inputBuffer.BufLen = len(inputBuffer.Buffer)
+
+	var statement Statement
+	result := PrepareStatement(inputBuffer, &statement)
+
+	if result != PrepareSuccess {
+		t.Errorf("result must be success")
+	}
+
+	if statement.Type != InsertStatement {
+		t.Errorf("statement type must be insert statement")
+	}
+
+	inputBuffer.Buffer = "select"
+	inputBuffer.BufLen = len(inputBuffer.Buffer)
+
+	result = PrepareStatement(inputBuffer, &statement)
+
+	if result != PrepareSuccess {
+		t.Errorf("result must be success")
+	}
+
+	if statement.Type != SelectStatement {
+		t.Errorf("statement type must be insert statement")
+	}
+
+	inputBuffer.Buffer = "unkown"
+	inputBuffer.BufLen = len(inputBuffer.Buffer)
+
+	result = PrepareStatement(inputBuffer, &statement)
+
+	if result != PrepareUnrecognizedStatement {
+		t.Errorf("result must be unrecognized statement")
+	}
+
+}
